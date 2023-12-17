510af0426a338325c4d832aee4a8531cfeb51f63
Adeniyii <ifedayoadeniyi@gmail.com>
2023-12-17 16:04:54 +0100

    feat: setup stripe schema with supabase

---
+++ migrations/0001_mysterious_glorian.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS "files" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"data" text,
+	"folder_id" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "folders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"data" text,
+	"workspace_id" uuid NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "files" ADD CONSTRAINT "files_folder_id_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "folders"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "folders" ADD CONSTRAINT "folders_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "workspaces"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
